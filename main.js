ded655c5f318124ed5fa8d471a970b0d0b366bdf
“alerenna” <alessandrorennafm@gmail.com>
2025-02-27 22:29:45 +0800

    Add print mail list DOM

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
+//Select the DOM nodes
+const listEl = document.querySelector('.email-list')
 
 //Inizializzo la lista dove caricare le 10 mail
 let listMail = []
 
 //Creo una funzione per generare la mail e pusharla nella array
-function randomEmail() {
+function randomEmailDom() {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(res => {
 
-            console.log(res);
-            console.log(res.data.response)
+            //console.log(res);
+            //console.log(res.data.response)
 
             listMail.push(res.data.response)
 
-            console.log(listMail);
+            listEl.innerHTML = ''
+
+            listMail.forEach((mail) => {
+
+                const listMail = document.createElement('li')
+                listMail.textContent = mail
+                listEl.appendChild(listMail)
+            })
 
         })
 }
 
 //Ciclo da 1 a 10 per far lavorare la funzione 10 volte e caricare la array lista con 10 email randomiche diverse 
 for (let i = 1; i <= 10; i++) {
-    randomEmail()
+    randomEmailDom()
 }
-console.log(listMail);
-
-
-//Stampo in pagina il contentuto della Array nella lista
-
-
 
 
 
